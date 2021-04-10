4d0073bbccc5b60f1506d9f7d58bb0b2bffcc88c
Craig Lennox <blipvert@blipvertica.com>
2021-04-10 13:17:05 -0400

    Added install.cmd script

---
+++ ICSharpCode.Decompiler.Console/install.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+rem Simple script to install the contents of a directory to a destination
+
+if "%1"=="" goto usage
+if not exist %1 goto usage
+if "%2"=="" goto usage
+if %2=="" goto usage
+
+if exist %2 rd /s /q %2
+xcopy /s /i %1 %2
+goto :eof
+
+:usage
+echo Usage: install SOURCEDIR DESTDIR
+exit /b 1
