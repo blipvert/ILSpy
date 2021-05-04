785c148f7e88af68df58b5610f0d7e8af58975f3
Craig Lennox <blipvert@blipvertica.com>
2021-05-04 12:27:53 -0400

    Write field, property, and method signatures of declaration nodes.
    This is a huge hack, poorly cribbed from CSharpOutputVisitor.

--- ICSharpCode.Decompiler/CSharp/OutputVisitor/XmlOutputVisitor.cs
+++ ICSharpCode.Decompiler/CSharp/OutputVisitor/XmlOutputVisitor.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -67,6 +67,28 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 		public void WriteSyntaxTree(SyntaxTree syntaxTree)
 		{
 			VisitNode(syntaxTree);
+		}
+
+		public override void VisitInvocationExpression(InvocationExpression invocationExpression)
+		{
+			base.VisitInvocationExpression(invocationExpression);
+		}
+		public override void VisitMethodDeclaration(MethodDeclaration methodDeclaration)
+		{
+			WriteComment(GetMethodSignature(methodDeclaration));
+			base.VisitMethodDeclaration(methodDeclaration);
+		}
+
+		public override void VisitPropertyDeclaration(PropertyDeclaration propertyDeclaration)
+		{
+			WriteComment(GetPropertySignature(propertyDeclaration));
+			base.VisitPropertyDeclaration(propertyDeclaration);
+		}
+
+		public override void VisitFieldDeclaration(FieldDeclaration fieldDeclaration)
+		{
+			WriteComment(GetFieldSignature(fieldDeclaration));
+			base.VisitFieldDeclaration(fieldDeclaration);
 		}
 
 		public override void VisitPrimitiveType(PrimitiveType primitiveType)
@@ -147,6 +169,73 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 		private void WriteComment(object comment)
 		{
 			writer.WriteComment(" " + comment.ToString().Replace(Environment.NewLine, " ") + " ");
+		}
+
+		private string GetFieldSignature(FieldDeclaration fieldDeclaration)
+		{
+			return GetEntitySignature(fieldDeclaration) + fieldDeclaration.GetSymbol().Name;
+		}
+
+		private string GetPropertySignature(PropertyDeclaration propertyDeclaration)
+		{
+			return GetEntitySignature(propertyDeclaration);
+		}
+
+		private string GetMethodSignature(MethodDeclaration methodDeclaration)
+		{
+			string methDecl = GetEntitySignature(methodDeclaration);
+			bool sep = false;
+
+			foreach (var tp in methodDeclaration.TypeParameters)
+			{
+				if (sep)
+					methDecl += ",";
+				else
+				{
+					methDecl += "<";
+					sep = true;
+				}
+				methDecl += tp.ToString();
+			}
+			if (sep)
+				methDecl += ">";
+			methDecl += "(";
+			sep = false;
+			foreach (var parm in methodDeclaration.Parameters)
+			{
+				if (sep)
+					methDecl += ",";
+				else
+					sep = true;
+				methDecl += parm.ToString();
+			}
+			methDecl += ")";
+			return methDecl;
+		}
+
+		private string GetEntitySignature(EntityDeclaration entityDeclaration)
+		{
+			string methDecl = "";
+			bool sep = false;
+			foreach (var attribute in entityDeclaration.Attributes)
+			{
+				if (sep)
+					methDecl += " ";
+				else
+					sep = true;
+				methDecl += attribute.ToString();
+			}
+			foreach (var modifier in entityDeclaration.ModifierTokens)
+			{
+				if (sep)
+					methDecl += " ";
+				else
+					sep = true;
+				methDecl += modifier.ToString();
+			}
+			if (sep)
+				methDecl += " ";
+			return methDecl + entityDeclaration.ReturnType.ToString() + " " + entityDeclaration.NameToken.ToString();
 		}
 	}
 }
