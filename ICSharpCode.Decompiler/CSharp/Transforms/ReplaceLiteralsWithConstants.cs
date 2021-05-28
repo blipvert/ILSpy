48fab3ca4d7205a0640be0844a84e6132a2c148b
Craig Lennox <blipvert@blipvertica.com>
2021-05-27 22:23:03 -0400

    Added using of System.Diagnostics needed for DebugDisplay.

--- ICSharpCode.Decompiler/CSharp/Transforms/ReplaceLiteralsWithConstants.cs
+++ ICSharpCode.Decompiler/CSharp/Transforms/ReplaceLiteralsWithConstants.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using ICSharpCode.Decompiler.CSharp.Syntax;
 using ICSharpCode.Decompiler.TypeSystem;
