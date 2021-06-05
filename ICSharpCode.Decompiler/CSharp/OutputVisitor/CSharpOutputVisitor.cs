f04304bf27850e28c8936c71717cb6306cb9b688
Craig Lennox <blipvert@blipvertica.com>
2021-06-05 17:28:39 -0400

    Removed now-unnecessary valueHint comment hack from CSharpOutputVisitor

--- ICSharpCode.Decompiler/CSharp/OutputVisitor/CSharpOutputVisitor.cs
+++ ICSharpCode.Decompiler/CSharp/OutputVisitor/CSharpOutputVisitor.cs
@@ -26,7 +26,6 @@ using ICSharpCode.Decompiler.CSharp.Syntax;
 using ICSharpCode.Decompiler.CSharp.Syntax.PatternMatching;
 using ICSharpCode.Decompiler.TypeSystem;
 using ICSharpCode.Decompiler.Util;
-using ICSharpCode.Decompiler.CSharp.Transforms;
 
 using Attribute = ICSharpCode.Decompiler.CSharp.Syntax.Attribute;
 
@@ -1126,12 +1125,6 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 		public virtual void VisitPrimitiveExpression(PrimitiveExpression primitiveExpression)
 		{
 			StartNode(primitiveExpression);
-			var valueHint = primitiveExpression.Annotation<ISymbolicContext>()?.RepresentationString;
-			if (!string.IsNullOrEmpty(valueHint))
-			{
-				writer.WriteComment(CommentType.MultiLine, valueHint);
-			}
-
 			writer.WritePrimitiveValue(primitiveExpression.Value, primitiveExpression.Format);
 			isAfterSpace = false;
 			EndNode(primitiveExpression);
